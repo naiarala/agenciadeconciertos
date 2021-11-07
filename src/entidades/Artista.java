e365cb3b347b45e4870f90f9cd740b8e49d208c6
naiarala <naiarala99@educastur.es>
2021-11-07 13:34:03 +0100

    Clases terminadas: Gira y Artista

--- src/entidades/Artista.java
+++ src/entidades/Artista.java
@@ -1,5 +1,65 @@
 package entidades;
 
-public class Artista {
+import java.util.Scanner;
 
+public class Artista {
+	private long id;
+	private String nombre;
+	private long géneromúsica;
+	private char letra;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public long getGéneromúsica() {
+		return géneromúsica;
+	}
+
+	public void setGénero(long géneromúsica) {
+		this.géneromúsica = géneromúsica;
+	}
+
+	public char getLetra() {
+		return letra;
+	}
+
+	public void setLetra(char letra) {
+		this.letra = letra;
+	}
+
+	public static <Artista> Artista newArtista() {
+		Artista art = newArtista();
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Introduzca el nombre del artista");
+		String nom = "";
+		nom = teclado.nextLine();
+		((entidades.Artista) art).setNombre(nom);
+		System.out.println(nom);
+
+		System.out.println("¿Son correctos los datos? \"true or false\"");
+		boolean s = true;
+		s = teclado.hasNextLine();
+		return art;
+	}
+
+	public Artista(long id, String nombre, long géneromúsica, char letra) {
+		this.id = id;
+		this.nombre = nombre;
+		this.géneromúsica = géneromúsica;
+		this.letra = letra;
+
+	}
 }
--- src/entidades/Gira.java
+++ src/entidades/Gira.java
@@ -1,9 +1,58 @@
 package entidades;
 
+import java.time.LocalDate;
+
 public class Gira {
 	public Concierto compone;
+
 	public Gira() {
 		compone = new Concierto(0, null, null);
 
 	}
+
+	private String nombre;
+	private LocalDate fechadeapertura;
+	private LocalDate fechadecierre;
+	private long id;
+
+	public Concierto getCompone() {
+		return compone;
+	}
+
+	public void setCompone(Concierto compone) {
+		this.compone = compone;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFechadeapertura() {
+		return fechadeapertura;
+	}
+
+	public void setFechadeapertura(LocalDate fechadeapertura) {
+		this.fechadeapertura = fechadeapertura;
+	}
+
+	public LocalDate getFechadecierre() {
+		return fechadecierre;
+	}
+
+	public void setFechadecierre(LocalDate fechadecierre) {
+		this.fechadecierre = fechadecierre;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 }
